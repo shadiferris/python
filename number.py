1427b3f19d846e14c1671791726a4d72f2e7427d
Shadi Ferris <sferris@au1.ibm.com>
2020-10-20 11:11:54 +1100

    Update number.py

--- number.py
+++ number.py
@@ -1,28 +1,11 @@
 import random
 
-guesses_made = 0
+def roll(min, max):
+    while True: 
+        print("Rolling the dices...")
+        print(f"Your number is {random.randint(min, max)}")
+        answer = input("Do you want to roll the dices again? (y/n) ")
+        if answer.lower() == "n":
+            break
 
-name = input('Hello! What is your name?\n')
-
-number = random.randint(1, 20)
-print ('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
-
-while guesses_made < 6:
-
-    guess = int(input('Take a guess: '))
-
-    guesses_made += 1
-
-    if guess < number:
-        print ('Your guess is too low.')
-
-    if guess > number:
-        print ('Your guess is too high.')
-
-    if guess == number:
-        break
-
-if guess == number:
-    print ('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
-else:
-    print ('Nope. The number I was thinking of was {0}'.format(number))
+roll(1, 6)

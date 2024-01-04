99b077f1cecbcae12ff0ba133d29c9d76cf9e084
Shadi Ferris <38468459+shadiferris@users.noreply.github.com>
2024-01-04 20:56:47 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+#beginning - just revisit simple basics
+''' age = input("How old are you? ")
+name = input("what is your name? ")
+age = int(age)
+name = str(name)
+print(f"{age}")
+print(f"{name}")
+
+val1 = 5
+val2 = 7
+donation = val1 + val2 + age
+print(f'Thanks {name}, I will received ${donation} in donations')
+'''
+
+#data type
+#int and float
+
+'''print(type(123))
+print(type(2 + 4))# int
+print(type(2 - 4)) # int
+print(type(2 * 4)) # int
+print(type(2 /4 )) # float
+
+print(2 ** 4) # 2 to the power of 4
+print(2 // 4) # rounded down operator to an integer
+print(5 % 4) # remainder / modular numbers.
+'''
+
+# Int and Float 
+# Maths functions - https://www.programiz.com/python-programming/modules/math
+'''print(round(5.9)) # round out the number to closest int
+print(abs(20)) # whats the absolute value of a number, ie no negative numbers
+'''
+
+# Operator precedence
+# Order of execution
+
+# 1. () brackets
+# 2. ** exponent
+# 3. * / // % (left to right)
+
+#binary representation 
+'''print(bin(5.5)) # changes the number to binary 
+'''
+
+#Variables
+

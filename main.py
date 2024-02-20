97d48205b3e18a447903a2b19f9e8bd96f2039c9
Shadi Ferris <38468459+shadiferris@users.noreply.github.com>
2024-02-20 21:59:48 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1703,3 +1703,24 @@ print(action_figure())
 print(action_figure['name'])
 
 '''
+# Exercise - Extending List
+'''
+
+class SuperList(list):
+
+    def __len__(self):
+        return 1000
+
+
+super_list1 = SuperList()
+print(len(super_list1))
+
+super_list1.append(5)
+print(super_list1[0])
+print(issubclass(SuperList, list))
+print(issubclass(list, object))
+'''
+
+
+
+

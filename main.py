d5589fd24656da41640f29f32a9c65815b7f46d6
Shadi Ferris <38468459+shadiferris@users.noreply.github.com>
2024-02-26 21:21:54 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1787,5 +1787,39 @@ print(C.num)
 #Functional Programming
 # https://en.wikipedia.org/wiki/History_of_programming_languages
 
+#Pure Functions
+'''
+def multiply_by2(li):
+  new_list = []
+  for item in li:
+    new_list.append(item*2)
+  return new_list
+
+print(multiply_by2([1,2,3]))
+'''
+
+# map()
+
+'''
+my_list = [1,2,3]
+def multiply_by2(item):
+  return item * 2
+
+print(list(map(multiply_by2, my_list)))
+print(my_list)
+#print(multiply_by2([1,2,3]))
+'''
+
+# Filer
+'''
+my_list = [1,2,3]
+def multiply_by2(item):
+  return item * 2
 
 
+def only_odd(item):
+  return item % 2 != 0
+
+print(list(filter(only_odd, my_list)))
+print(my_list)
+'''

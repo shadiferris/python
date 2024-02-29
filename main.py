c5d9373df3415f9bb9e53b787a9c9431a241d58b
Shadi Ferris <38468459+shadiferris@users.noreply.github.com>
2024-02-29 21:48:36 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1823,3 +1823,41 @@ def only_odd(item):
 print(list(filter(only_odd, my_list)))
 print(my_list)
 '''
+
+#Zip
+'''
+my_list = [1,2,3]
+your_list = [10,20,30,40]
+their_list = ["dad", "mum", "son"]
+def multiply_by2(item):
+  return item * 2
+
+
+def only_odd(item):
+  return item % 2 != 0
+
+print(list(zip(my_list,your_list, their_list)))
+print(my_list)
+print(your_list)
+print(their_list)
+'''
+#Reduce
+'''
+from functools import reduce
+
+my_list = [1,2,3]
+
+def multiply_by2(item):
+  return item * 2
+
+def only_odd(item):
+  return item % 2 != 0
+
+def accumulator(acc, item):
+  print(acc, item)
+  return acc + item
+
+print(reduce(accumulator, my_list, 10))
+print(my_list)
+'''
+

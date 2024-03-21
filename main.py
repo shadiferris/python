80f882a813fdb7dfbefee76037ccb86f5608ff17
Shadi Ferris <38468459+shadiferris@users.noreply.github.com>
2024-03-21 20:42:19 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2168,5 +2168,70 @@ while True:
     print("ok, Im finally done")
 '''
 
+#Generators
+'''
+range(100)
+list(range(100))
 
+def make_list(num):
+  result = []
+  for i in range(num):
+    result.append(i*2)
+  return result
 
+new_li = make_list(4)
+print(new_li)
+'''
+
+#Generators 2
+# iterable - something to loop over and actions in between.
+'''
+def generator_function(num):
+  for i in range(num):
+    yield i
+
+#for item in generator_function(10):
+#  print(item)
+
+g = generator_function(2)
+
+next(g)
+next(g)
+print(next(g))
+'''
+
+# Generator Performance
+
+#generators are much more performant than lists. (i.e range > list in performance.)
+
+#So generators are really, really useful when calculating large sets of data.
+'''
+from time import time
+
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args, *kwargs)
+        t2 = time()
+        print(f'took {t2-t1} s')
+        return result
+    return wrapper
+
+@performance
+def long_time():
+    print('1')
+    for i in range(1000000): #it finishes after.
+        i*5
+
+long_time()
+print()
+
+@performance
+def long_time2():
+    print('2')
+    for i in list(range(1000000)): #it took longer.
+        i*5
+
+long_time2()
+print()
+'''

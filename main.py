df0fde470ed2ca335f4651db3c183405043bbb4b
Shadi Ferris <shadiferris@hotmail.com>
2024-04-05 21:06:09 +1100

    new files

--- main.py
+++ main.py
@@ -2350,3 +2350,35 @@ print(sys)
 sys.argv
 
 '''
+
+# Exercise Guessing Game:
+
+from random import randint
+import sys
+
+first = sys.argv[1]
+
+last = sys.argv[2]
+
+answer = randint(first, last)
+
+
+
+while True:
+    try:
+        print(answer)
+        guess = int(input(f"Guess a number between {first} and {last} "))
+        if first < guess < last:
+          #print("things are working")
+          if answer == guess:
+            print("you successfully guessed the number.. well done")
+            break
+          else:
+            print("number was incorrect, please select another number")
+            continue
+        else:
+           print("number outside range, please select another number")
+           continue
+    except ValueError:
+        print("please enter a number")
+        continue

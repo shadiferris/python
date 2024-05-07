fc2304d4a67a712e200aa55e10ceebb8cf1556f7
Shadi Ferris <shadiferris@hotmail.com>
2024-05-07 21:22:12 +1000

    image processing

--- image1.py
+++ image1.py
@@ -11,5 +11,16 @@ print(im.format, im.size, im.mode)
 #out = im.filter(ImageFilter.SHARPEN)
 #out.save("./images/sharpen.png", 'png')
 
-out = im.convert('L')
-out.save("./images/grey_scale.png", 'png')
+#out = im.convert('L')
+#out.save("./images/grey_scale.png", 'png')
+
+#im.show()
+#out = im.convert('L')
+#crooked_im = out.rotate(90)
+#crooked_im.save("./images/crooked_greyscale.png", 'png')
+#crooked_im.show()
+
+box = (100, 100, 400, 400)
+cropped_im = im.crop(box)
+cropped_im.save("./images/cropped_im.png", 'png')
+cropped_im.show()

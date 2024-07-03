a83aef55f61a9bbdcac6f03d90f0f9d8ec5f67c7
Shadi Ferris <shadiferris@hotmail.com>
2024-07-03 20:42:47 +1000

    scrape

--- scrape.py
+++ scrape.py
@@ -2,8 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
-r = requests.get('https://news.ycombinator.com/news', auth=('user', 'pass'))
-print(f"this is {r.text}")
+res = requests.get('https://news.ycombinator.com/news', auth=('user', 'pass'))
+#print(f"this is {r.text}")
 #r.status_code
 #print(r.status_code)
 #print(r.headers['content-type'])
+
+soup = BeautifulSoup(res.text, 'html.parser')
+#print(soup.prettify())
+#print(soup.find_all('div'))
+#print(soup.find_all('title'))
+# https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Selectors
+#print(soup.select("#up_40862436"))
+links = soup.select(".sitebit")
+votes = soup.select(".score")
+
+print(votes[0])

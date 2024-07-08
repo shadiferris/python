99dbee5d56fbfa37d01a4b09373cb29a4dbf3d6a
Shadi Ferris <shadiferris@hotmail.com>
2024-07-08 22:54:18 +1000

    scrape

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 
 res = requests.get('https://news.ycombinator.com/news', auth=('user', 'pass'))
@@ -16,5 +17,37 @@ soup = BeautifulSoup(res.text, 'html.parser')
 #print(soup.select("#up_40862436"))
 links = soup.select(".sitebit")
 votes = soup.select(".score")
+subtext = soup.select(".subtext")
 
-print(votes[0])
+#print(votes[0]
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        #href = links[idx].get('href', None)
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 100:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    pprint.pprint(hn)
+
+
+def create_custom_hn2(links, subtext):
+  hn = []
+  for idx, item in enumerate(links):
+    title = item.getText()
+    href = item.get('href', None)
+    vote = subtext[idx].select('.score')
+    if len(vote):
+      points = int(vote[0].getText().replace(' points', ''))
+      if points > 99:
+        hn.append({'title': title, 'link': href, 'votes': points})
+  pprint.pprint(hn)
+  #return sort_stories_by_votes(hn)
+
+
+
+create_custom_hn2(links, subtext)
+

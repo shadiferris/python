3649868b16ca3542fc43877e419dd2d7084d9d72
Shadi Ferris <shadiferris@hotmail.com>
2024-08-07 21:38:00 +1000

    web server

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,2 +1,10 @@
 #
 # Flask - https://flask.palletsprojects.com/en/1.1.x/
+
+from flask import Flask
+app = Flask(__name__)
+#print(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! this is a TEST' 

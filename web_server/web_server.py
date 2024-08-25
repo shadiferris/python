921989101144bdf2b61170bd9fa14e81cde8d100
Shadi Ferris <shadiferris@hotmail.com>
2024-08-25 22:08:20 +1000

    more web_server changes

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -33,9 +33,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/hello/')
+@app.route('/about.html')
 def hello(name=None):
-    return render_template('index.html', name=name)
+    return render_template('about.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)

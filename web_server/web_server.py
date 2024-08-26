ee3b4111ec46b9498de950e8f0c853437dc98930
Shadi Ferris <shadiferris@hotmail.com>
2024-08-26 20:52:53 +1000

    favicon and other web_server changes

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,31 +1,15 @@
 #
 # Flask - https://flask.palletsprojects.com/en/1.1.x/
-'''
-from flask import Flask
-from flask import render_template
+# how to run and execute flask
+# https://flask.palletsprojects.com/en/2.0.x/quickstart/
+# Rendering templates
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
+# static files
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#static-files
 
-app = Flask(__name__)
-#print(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('./templates/index.html')
-
-@app.route('/hello/')
-@app.route('/hello/')
-def hello(name=None):
-    return render_template('./index.html', name=name)
-
-@app.route('/blog')
-def blog(name=None):
-    return 'these are my thoughts on blog'
-
-@app.route('/blog/2020/dogs')
-def blog2(name=None):
-    return 'dog page'
-'''
-
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -36,6 +20,7 @@ def hello_world():
 @app.route('/about.html')
 def hello(name=None):
     return render_template('about.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
